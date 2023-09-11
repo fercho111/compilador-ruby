5c39622e8280c08cf1f3de9503a2322c0e4fa3ab
josedjaykv <josedjaykv@gmail.com>
2023-09-11 10:12:38 -0500

    Listo Parentesis

--- lpp/repl.rb
+++ lpp/repl.rb
@@ -1,18 +1,23 @@
 require_relative 'lexer'
 require_relative 'tokens'
-
-# EOF_TOKEN = Token.new(TokenType::EOF, '')
+require_relative 'parser'
 
 def start_repl
-    loop do
-        print '>> '
-        source = gets.chomp
-        break if source == "salir()"
-        lexer = Lexer.new(source)
-        loop do
-            token = lexer.next_token
-            break if token.token_type == :EOF
-            puts token
-        end
+  loop do
+    print '>> '
+    source = gets.chomp
+    break if source == "salir()"
+
+    lexer = Lexer.new(source)
+    parser = Parser.new(lexer)
+
+    begin
+      result = parser.parse
+      puts "Resultado: #{result}"
+    rescue StandardError => e
+      puts "Error: #{e.message}"
     end
+  end
 end
+
+start_repl
