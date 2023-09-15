086606778fab0d69a679aca86be4262583b0fe5a
Fer <ferchogomez545@gmail.com>
2023-09-15 11:49:19 -0500

    ahi vamos

--- lpp/parser.rb
+++ lpp/parser.rb
@@ -34,6 +34,11 @@ class Parser
     @current_token = nil
     @peek_token = nil
     @errors = []
+
+    @prefix_parse_fns = register_prefix_fns
+    @infix_parse_fns = register_infix_fns
+    
+
   end
 
   def parse_program
@@ -49,9 +54,6 @@ class Parser
     program
   end
 
-  def parse
-    parse_expression
-  end
 
   private
 
@@ -106,12 +108,13 @@ class Parser
   def parse_call_arguments
     arguments = []
     # assert
+
     if @peek_token.token_type == TokenType::RPAREN
       advance_tokens
       return arguments
     end
     advance_tokens
-    if (expression = self._parse_expression(Precedence::LOWEST))
+    if (expression = parse_expression(Precedence::LOWEST))
       arguments.push(expression)
     end
 
@@ -132,22 +135,19 @@ class Parser
 
   end
 
-  def parse_expression
-    left = parse_term
-  
-    while [TokenType::PLUS, TokenType::MINUS].include?(@current_token.token_type)
-      operator = @current_token
-      consume(operator.token_type)
-      right = parse_term
-  
-      if operator.token_type == TokenType::PLUS
-        left += right
-      else
-        left -= right
-      end
+  def parse_expression(precedence)
+    # assert
+    
+    prefix_parse_fn = @prefix_parse_fns[@current_token.token_type]
+    if prefix_parse_fn == nil
+      message = 'No se encontro ninguna funcion para parsear #{@current_token.literal}'
+      @errors.push(message)
+      return
     end
-  
-    left
+    left_expression = prefix_parse_fn
+    # assert
+    while !(@peek_token.token_type == TokenType::SEMICOLON) && precedence < peek
+
   end
   
   def parse_term
@@ -213,6 +213,14 @@ class Parser
 
 
 
+
+  def parse_prefix_expression
+    #assert
+    prefix_expression = Prefix.new(@current_token, @current_token.literal)
+    advance_tokens
+    prefix_expression.right = parse_expression(Precedence::PREFIX)
+    return prefix_expression
+
   def parse_infix_expression(left)
     # raise 'Assertion error' if !@current_token.nil?
     #
@@ -228,6 +236,52 @@ class Parser
 
   end
 
+  def parse_return_statement
+    #assert
+    return_statement = ReturnStatement.new(@current_token)
+    advance_tokens
+    return_statement.return_value = parse_expression(Precedence::LOWEST)
+    # assert
+    if @peek_token.token_type == TokenType::SEMICOLON
+      advance_tokens
+    end
+    return return_statement
+  end
+
+  def parse_statement
+    #assert
+    if @current_token.token_type == TokenType::LET
+      return parse_let_statement
+    elsif @current_token.token_type == TokenType::RETURN
+      return parse_return_statement
+    else
+      return parse_expression_statement
+    end
+  end
+  
+  def peek_precedence
+    # assert
+    precedence = PRECEDENCES[@peek_token.token_type]
+    if precedence == nil
+      return Precedence::LOWEST
+    end
+    return precedence
+  end
+
+  def register_prefix_fns
+    {
+      TokenType::FALSE => method(:parse_boolean),
+      TokenType::FUNCTION => method(:parse_function),
+      TokenType::IDENT => method(:parse_identifier),
+      TokenType::IF => method(:parse_if),
+      TokenType::INT => method(:parse_integer),
+      TokenType::LPAREN => method(:parse_grouped_expression),
+      TokenType::MINUS => method(:parse_prefix_expression),
+      TokenType::NEGATION => method(:parse_prefix_expression),
+      TokenType::TRUE => method(:parse_boolean),
+    }
+  end
+
   def register_infix_fns
     {
       TokenType::PLUS => method(:parse_infix_expression),
@@ -241,6 +295,5 @@ class Parser
       TokenType::LPAREN => method(:parse_call)
     }
   end
-  
   
 end
